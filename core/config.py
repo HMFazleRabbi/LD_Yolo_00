0e042b806c1f24876197443a6f8298e369c60e44
gan-xiao-nan <tr_xiao-nan.gan@vie.com.my>
2020-07-29 01:41:47 +0000

    updated config

--- core/config.py
+++ core/config.py
@@ -38,13 +38,13 @@ __C.TRAIN                       = edict()
 
 __C.TRAIN.ANNOT_PATH            = "./data/dataset/Exp-02/train.txt"
 __C.TRAIN.BATCH_SIZE            = 1
-__C.TRAIN.INPUT_SIZE            = [1024, 960, 896, 832, 768, 704]
+__C.TRAIN.INPUT_SIZE            = [448,416,384,352,320,288]
 __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
 __C.TRAIN.WARMUP_EPOCHS         = 2
-__C.TRAIN.FISRT_STAGE_EPOCHS    = 20
-__C.TRAIN.SECOND_STAGE_EPOCHS   = 30
+__C.TRAIN.FISRT_STAGE_EPOCHS    = 30
+__C.TRAIN.SECOND_STAGE_EPOCHS   = 40
 __C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/Pretrained_Weight/yolov3_coco_demo.ckpt"
 __C.TRAIN.OUTPUT_WEIGHT         = "./checkpoint/Exp-2-0"
 
