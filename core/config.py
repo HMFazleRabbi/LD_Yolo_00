1af9a699f25932bc15105ed87c6995b43c70eef2
HMFazleRabbi <55730363+HMFazleRabbi@users.noreply.github.com>
2020-09-08 15:30:15 +0800

    Added train directory support

--- core/config.py
+++ core/config.py
@@ -23,7 +23,7 @@ cfg                             = __C
 __C.YOLO                        = edict()
 
 # Set the class name
-__C.YOLO.CLASSES                = "./data/classes/component.names"
+__C.YOLO.CLASSES                = "./data/classes/pad.names"
 __C.YOLO.ANCHORS                = "./data/anchors/basline_anchors.txt"
 __C.YOLO.MOVING_AVE_DECAY       = 0.9995
 __C.YOLO.STRIDES                = [8, 16, 32]
@@ -36,32 +36,32 @@ __C.YOLO.DEMO_WEIGHT            = "./checkpoint/Pretrained_Weight/yolov3_coco_pr
 # Train options
 __C.TRAIN                       = edict()
 
-__C.TRAIN.ANNOT_PATH            = "./data/dataset/Exp-07/train.txt"
-__C.TRAIN.BATCH_SIZE            = 1
-__C.TRAIN.INPUT_SIZE            = [1024]
+__C.TRAIN.ANNOT_PATH            = "./data/dataset/Exp-08/train.txt"
+__C.TRAIN.BATCH_SIZE            = 2
+__C.TRAIN.INPUT_SIZE            = [928,960,992,1024,1056,1088]
 __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
-__C.TRAIN.WARMUP_EPOCHS         = 5
-__C.TRAIN.FISRT_STAGE_EPOCHS    = 10
-__C.TRAIN.SECOND_STAGE_EPOCHS   = 30
-__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/Exp-7-2/Exp-7-2_test_loss=1540.0201.ckpt-30" #"./checkpoint/Pretrained_Weight/yolov3_coco_pretrained.ckpt"
-__C.TRAIN.LOGDIR                = "./data/log/train"
-__C.TRAIN.OUTPUT_WEIGHT         = "./checkpoint/Exp-7-2-01"
+__C.TRAIN.WARMUP_EPOCHS         = 10
+__C.TRAIN.FISRT_STAGE_EPOCHS    = 50
+__C.TRAIN.SECOND_STAGE_EPOCHS   = 50
+__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/Exp-8-A-01/Exp-8-A-01_test_loss=169.7120.ckpt-20" 
+__C.TRAIN.LOGDIR                = "./data/log/Exp-8-A-08"
+__C.TRAIN.OUTPUT_WEIGHT         = "./checkpoint/Exp-8-A-08"
 
 
 
 # TEST options
 __C.TEST                        = edict()
 
-__C.TEST.ANNOT_PATH             = "./data/dataset/Exp-07/test.txt"
+__C.TEST.ANNOT_PATH             = "./data/dataset/Exp-08/test.txt"
 __C.TEST.BATCH_SIZE             = 1
-__C.TEST.INPUT_SIZE             = 2048
+__C.TEST.INPUT_SIZE             = 1024
 __C.TEST.DATA_AUG               = False
 __C.TEST.WRITE_IMAGE            = True
-__C.TEST.WRITE_IMAGE_PATH       = "./data/detection/Exp-7-2/test/"
+__C.TEST.WRITE_IMAGE_PATH       = "./data/detection/Exp-8-A-08/test/"
 __C.TEST.WRITE_IMAGE_SHOW_LABEL = False
-__C.TEST.WEIGHT_FILE            = "./checkpoint/Exp-7-2/Exp-7-2_test_loss=1560.9564.ckpt-28"
+__C.TEST.WEIGHT_FILE            = "./checkpoint/Exp-8-A-08/Exp-8-A-03_test_loss=14.0184.ckpt-28"
 __C.TEST.SHOW_LABEL             = False
 __C.TEST.SCORE_THRESHOLD        = 0.60
 __C.TEST.IOU_THRESHOLD          = 0.45
